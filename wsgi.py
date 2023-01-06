2fd3a63e83ba4d05038efd794d0608d477649534
Воронов Егор <vip.egor.voronov.2016@mail.ru>
2023-01-06 23:47:13 +0300

    route calculator for 3 nums

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/Index')
-def index():
-    return 'Hello world'
+@app.route('/<string:exp>')
+def calc(exp):
+    a = exp[1]
+
+    x, y = int(exp[0]), int(exp[2])
+
+    if a == ':':
+        return f'{x / y}'
+
+    elif exp[1] == '*' and not (exp[2] == '*'):
+        return f'{x * y}'
+
+    elif a == '+':
+        return f'{x + y}'
+
+    elif a == '-':
+        return f'{x - y}'
+
+    elif exp[1] == '*' and exp[2] == '*':
+        return f'{x ** y}'
 
 
 if __name__ == '__main__':
