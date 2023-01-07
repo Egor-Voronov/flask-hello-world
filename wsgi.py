329e38ac24aeddbce4eb40bfe7a4134941a1d14e
Воронов Егор <vip.egor.voronov.2016@mail.ru>
2023-01-08 00:07:16 +0300

    jinja 2 templates

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<float:num>/')
+def index(num):
+    return render_template('index.html',
+                          number = num * 2,
+                          text = f"Ваше число {num}, умноженное на 2:",
+                          )
 
 
 if __name__ == '__main__':
