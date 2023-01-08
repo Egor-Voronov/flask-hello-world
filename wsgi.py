e3827ff39af50f325e470c8d323844f5d7a688f2
Воронов Егор <vip.egor.voronov.2016@mail.ru>
2023-01-08 15:36:40 +0300

    get + post methods

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
 from flask import render_template
 
 
-@app.route('/<float:a>/')
-def index(a):
+@app.route('/', methods=['get', 'post'])
+def index():
+    server_message = ''
+    client_message = ''
+
+    if request.method == 'POST':
+        client_message = request.form.get('message')
+
+    if client_message == 'hi':
+        server_message = 'hello'
+    elif client_message != '' :
+        server_message = 'how are you?'
+
     return render_template('index.html',
-                           r=a,
-                           pi=3.14)
+                           message = server_message
+                           )
 
 if __name__ == '__main__':
     app.run(debug=True)
